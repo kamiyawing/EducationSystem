26f3fb1cf3eeec1ced132dfad6be1244b214a9b4
黒田千寛 <kurodachihiro@MacBookAir.AirPort>
2025-05-13 16:00:41 +0900

    https://github.com/kamiyawing/EducationSystem.git

--- app/Http/Controllers/User/CurriculumController.php
+++ app/Http/Controllers/User/CurriculumController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Curriculum;
+use Carbon\Carbon;
 
 
 class CurriculumController extends Controller
@@ -12,14 +13,19 @@ class CurriculumController extends Controller
 
 public function index(Request $request)
 {
-    $grade = $request->input('grade_id');
-    $month = $request->input('month', now()->month); // 初期値は今月
+    $grades = config('grades.grades');
+    $gradeId = $request->input('grade_id');
+    $gradeColors = config('grades.gradeColors');
     $year = $request->input('year', now()->year);
+    $month = $request->input('month', now()->month); // 初期値は今月
+    $date = Carbon::createFromDate($year, $month, 1);
+    $prev = $date->copy()->addMonth();
+    $next = $date->copy()->addMonth();
 
     $curriculums = Curriculum::with('deliveryTimes')
        ->where(function ($query) use ($month, $year){
         //常時フラグオン時 
-        $query->where('always_delivery_flg', 1)
+        $query->where('always_delivery_flg', true)
 
 
         //該当付き設定
@@ -28,32 +34,17 @@ public function index(Request $request)
                   ->whereMonth('delivery_from', $month);
             });
         }) 
-        ->when($grade, function ($query) use ($grade) {
-            $query->where('grade_id', $grade);
+        ->when($gradeId, function ($query, $gradeId) {
+            return $query->where('grade_id', $gradeId);
         })
         ->get();
 
-
-                        $grades = [
-                            1 => '小学生1年生',
-                            2 => '小学生2年生',
-                            3 => '小学生3年生',
-                            4 => '小学生4年生',
-                            5 => '小学生5年生',
-                            6 => '小学生6年生',
-                            7 => '中学生1年生',
-                            8 => '中学生2年生',
-                            9 => '中学生3年生',
-                            10 => '高校生1年生',
-                            11 => '高校生2年生',
-                            12 => '高校生3年生',
-                            ];
-
-    return view('user/curriculum_list', compact('curriculums', 'grade', 'month'));
+    return view('user/curriculum_list', compact('curriculums', 'month', 'year', 'date', 'prev', 'next', 'grades', 'gradeColors', 'gradeId'));
 }
 
 public function deliveryTimes()
 {
     return $this->hasMany(DeliveryTime::class, 'curriculums_id');
 }
+
 }
--- resources/views/user/curriculum_list.blade.php
+++ resources/views/user/curriculum_list.blade.php
@@ -2,45 +2,6 @@
 
 @section('content')
 
-@php
-     $grades = [
-        1 => '小学生1年生',
-        2 => '小学生2年生',
-        3 => '小学生3年生',
-        4 => '小学生4年生',
-        5 => '小学生5年生',
-        6 => '小学生6年生',
-        7 => '中学生1年生',
-        8 => '中学生2年生',
-        9 => '中学生3年生',
-        10 => '高校生1年生',
-        11 => '高校生2年生',
-        12 => '高校生3年生',
-        ];
-
-     $gradeColors = [
-        1 => 'info',
-        2 => 'info',
-        3 => 'info',
-        4 => 'info',
-        5 => 'info',
-        6 => 'info',
-        7 => 'primary',
-        8 => 'primary',
-        9 => 'primary',
-        10 => 'success',
-        11 => 'success',
-        12 => 'success',
-        ];
-
-        $currentMonth = request('month') ?? now()->month;
-        $currentYear = request('year') ?? now()->year;
-
-        $date = \Carbon\Carbon::createFromDate($currentYear, $currentMonth, 1);
-
-        $prev = $date->copy()->subMonth();
-        $next = $date->copy()->addMonth();
-        @endphp
 
 {{-- 戻るボタン --}}
     <a href="{{ route('user.top') }}" class="btn btn-secondary">戻る</a>
@@ -98,7 +59,7 @@
         <div class="row">
             @foreach($curriculums as $curriculum)
                 {{-- 常時フラグと学年一致で表示させるとこ --}}
-                @if($curriculum->always_delivery_flg == 1 || $curriculum->grade_id == request('grade_id'))
+                @if($curriculum->always_delivery_flg === true || $curriculum->grade_id == request('grade_id'))
                 <div class="col-md-4 mb-4">
                     <div class="card h-100">
                         {{-- サムネイル --}}
@@ -116,7 +77,7 @@
                             {{--日時表示 --}}
                             @foreach($curriculum->deliveryTimes as $time)
                                <p class="card-text">
-                                   配信期間：{{ c::parse($time->delivery_from)->format('Y年m月d日 H:i') }}
+                                   配信期間：{{ \Carbon\Carbon::parse($time->delivery_from)->format('Y年m月d日 H:i') }}
                                    ~ {{ \Carbon\Carbon::parse($time->delivery_to)->format('Y年m月d日 H:i') }}
                                </p>
                             @endforeach
